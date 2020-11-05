2261ae87b3e7245d654b51b4014d1ccd639c40fd
jucca182 <69469306+jucca182@users.noreply.github.com>
2020-11-05 20:17:26 -0300

    novos estudos

---
+++ fatorial/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fatorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int fatorial =  1;
+
+            for (int contador=1; contador<=10;contador++)
+            {
+                fatorial = fatorial * contador;
+                Console.WriteLine("O fatorial de : "+contador+" é  "+fatorial);
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ lacosWhile/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lacosWhile
+{
+    class Programa
+    {
+        static void Main(String[] args)
+        {
+            for (int fator1 = 1; fator1<=10;fator1++)
+            {
+                for (int fator2 = 1;fator2<=10;fator2++)
+                {
+                    Console.WriteLine(fator1 +" * "+fator2+" = "+ fator1*fator2);
+                    Console.WriteLine();
+                }
+            }
+               
+
+            Console.ReadLine();
+        }
+
+    }
+}
